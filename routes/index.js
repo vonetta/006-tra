56f0c8f0bf1838b93ec2a733f2aebd1ee246908d
Vonetta <vonettastevenson@gmail.com>
2017-07-10 22:00:36 -0700

    added learning file

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,23 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  const player = { name: 'Kobe', age: 40, good: true };
-  res.json(req.query.name);
+    const player = { name: 'Kobe', age: 40, good: true };
+    // res.send('Hey! It works!');
+    // res.json(player);
+
+    res.send(req.query.name);
 });
 
 router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
+    const reverse = [...req.params.name].reverse().join('');
+    res.send(reverse);
 });
 
 router.get('/learning', (req, res) => {
-  res.render('learning', {
-    title: 'Latest Store Closings'
-  });
+    res.render('learning', {
+        title: 'Pug is Fun!',
+        condition: true,
+    });
 });
 
 module.exports = router;
