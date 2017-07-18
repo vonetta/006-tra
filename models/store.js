169cdbd326f213f7d053665b55ab520c280a22a4
Vonetta <vonettastevenson@gmail.com>
2017-07-17 20:10:06 -0700

    complete notes for create store model

--- models/store.js
+++ models/store.js
@@ -2,6 +2,29 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
-const storeSchema = new mongoose.Schema({});
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name',
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true,
+  },
+  tags: [String],
+});
+
+storeSchema.pre('save', function(next) {
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; // stop this function from running
+    // could also just type return next();
+  }
+  this.slug = slug(this.name);
+  next();
+  //  Todo make more resilient so slugs are unique
+});
 
 module.exports = mongoose.model('Store', storeSchema);
