a62f8eb917d0c8c1014f77764a25883b309c250e
Vonetta <vonettastevenson@gmail.com>
2017-08-21 20:58:31 -0700

    complete upload-resize

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,16 @@ const router = express.Router();
 router.get('/', storeController.homePage);
 // show all stores on stores page
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+
+// router.post('/add', catchErrors(storeController.createStore// add this chunk of code
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+);
+
 router.post('/add/:id', catchErrors(storeController.updateStore));
 
 module.exports = router;
